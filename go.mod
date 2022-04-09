f805b50702f702d002f25f9900cc56d3fc7716ed
jsd1982 <james.jdunne@gmail.com>
2022-04-09 15:10:57 -0500

    Simple headless emulator which stops on WDM #$FF and takes WRAM snapshots on any other WDM value > 10

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/alttpo/alttpemu
 
 go 1.17
 
-require (
-	github.com/alttpo/snes v0.0.0-20220404042428-a485ab43ed77
-	golang.org/x/image v0.0.0-20220321031419-a8550c1d254a
-)
+require github.com/alttpo/snes v0.0.0-20220404042428-a485ab43ed77
---
+++ main.go
@@ -0,0 +1,596 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"github.com/alttpo/snes/asm"
+	"github.com/alttpo/snes/emulator/bus"
+	"github.com/alttpo/snes/emulator/cpu65c816"
+	"github.com/alttpo/snes/emulator/memory"
+	"github.com/alttpo/snes/mapping/lorom"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func newEmitterAt(s *System, addr uint32, generateText bool) *asm.Emitter {
+	lin, _ := lorom.BusAddressToPak(addr)
+	a := asm.NewEmitter(s.ROM[lin:], generateText)
+	a.SetBase(addr)
+	return a
+}
+
+func main() {
+	var err error
+
+	var f *os.File
+	f, err = os.Open("alttp-jp.sfc")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var s *System
+
+	// create the CPU-only SNES emulator:
+	s = &System{
+		Logger:    os.Stdout,
+		LoggerCPU: nil,
+	}
+	if err = s.CreateEmulator(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	_, err = f.Read(s.ROM[:])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// start from RESET vector:
+	s.CPU.Reset()
+	frame := uint64(0)
+	for {
+		// execute until WDM is hit, then take a WRAM snapshot:
+		if err = s.Exec(0); err != nil {
+			fmt.Println(err)
+			return
+		}
+		ioutil.WriteFile(fmt.Sprintf("data/%d.wram", frame), s.WRAM[:], 0644)
+
+		if s.CPU.WDM == 0xFF {
+			break
+		} else {
+			fmt.Printf("WDM #$%02x\n", s.CPU.WDM)
+		}
+
+		frame++
+	}
+}
+
+func read16(b []byte, addr uint32) uint16 {
+	return binary.LittleEndian.Uint16(b[addr : addr+2])
+}
+
+func read8(b []byte, addr uint32) uint8 {
+	return b[addr]
+}
+
+func write8(b []byte, addr uint32, value uint8) {
+	b[addr] = value
+}
+
+func write16(b []byte, addr uint32, value uint16) {
+	binary.LittleEndian.PutUint16(b[addr:addr+2], value)
+}
+
+func write24(b []byte, addr uint32, value uint32) {
+	binary.LittleEndian.PutUint16(b[addr:addr+2], uint16(value&0x00FFFF))
+	b[addr+3] = byte(value >> 16)
+}
+
+type System struct {
+	// emulated system:
+	Bus *bus.Bus
+	CPU *cpu65c816.CPU
+	*HWIO
+
+	ROM  [0x1000000]byte
+	WRAM [0x20000]byte
+	SRAM [0x10000]byte
+
+	VRAM [0x10000]byte
+
+	Logger    io.Writer
+	LoggerCPU io.Writer
+}
+
+func (s *System) CreateEmulator() (err error) {
+	// create primary A bus for SNES:
+	s.Bus, _ = bus.NewWithSizeHint(0x40*2 + 0x10*2 + 1 + 0x70 + 0x80 + 0x70*2)
+	// Create CPU:
+	s.CPU, _ = cpu65c816.New(s.Bus)
+
+	// map in ROM to Bus; parts of this mapping will be overwritten:
+	for b := uint32(0); b < 0x40; b++ {
+		halfBank := b << 15
+		bank := b << 16
+		err = s.Bus.Attach(
+			memory.NewRAM(s.ROM[halfBank:halfBank+0x8000], bank|0x8000),
+			"rom",
+			bank|0x8000,
+			bank|0xFFFF,
+		)
+		if err != nil {
+			return
+		}
+
+		// mirror:
+		err = s.Bus.Attach(
+			memory.NewRAM(s.ROM[halfBank:halfBank+0x8000], (bank+0x80_0000)|0x8000),
+			"rom",
+			(bank+0x80_0000)|0x8000,
+			(bank+0x80_0000)|0xFFFF,
+		)
+		if err != nil {
+			return
+		}
+	}
+
+	// SRAM (banks 70-7D,F0-FF) (7E,7F) will be overwritten with WRAM:
+	for b := uint32(0); b < uint32(len(s.SRAM)>>15); b++ {
+		bank := b << 16
+		halfBank := b << 15
+		err = s.Bus.Attach(
+			memory.NewRAM(s.SRAM[halfBank:halfBank+0x8000], bank+0x70_0000),
+			"sram",
+			bank+0x70_0000,
+			bank+0x70_7FFF,
+		)
+		if err != nil {
+			return
+		}
+
+		// mirror:
+		err = s.Bus.Attach(
+			memory.NewRAM(s.SRAM[halfBank:halfBank+0x8000], bank+0xF0_0000),
+			"sram",
+			bank+0xF0_0000,
+			bank+0xF0_7FFF,
+		)
+		if err != nil {
+			return
+		}
+	}
+
+	// WRAM:
+	{
+		err = s.Bus.Attach(
+			memory.NewRAM(s.WRAM[0:0x20000], 0x7E0000),
+			"wram",
+			0x7E_0000,
+			0x7F_FFFF,
+		)
+		if err != nil {
+			return
+		}
+
+		// map in first $2000 of each bank as a mirror of WRAM:
+		for b := uint32(0); b < 0x70; b++ {
+			bank := b << 16
+			err = s.Bus.Attach(
+				memory.NewRAM(s.WRAM[0:0x2000], bank),
+				"wram",
+				bank,
+				bank|0x1FFF,
+			)
+			if err != nil {
+				return
+			}
+		}
+		for b := uint32(0x80); b < 0x100; b++ {
+			bank := b << 16
+			err = s.Bus.Attach(
+				memory.NewRAM(s.WRAM[0:0x2000], bank),
+				"wram",
+				bank,
+				bank|0x1FFF,
+			)
+			if err != nil {
+				return
+			}
+		}
+	}
+
+	// Memory-mapped IO registers:
+	{
+		s.HWIO = &HWIO{s: s}
+		for b := uint32(0); b < 0x70; b++ {
+			bank := b << 16
+			err = s.Bus.Attach(
+				s.HWIO,
+				"hwio",
+				bank|0x2000,
+				bank|0x7FFF,
+			)
+			if err != nil {
+				return
+			}
+
+			bank = (b + 0x80) << 16
+			err = s.Bus.Attach(
+				s.HWIO,
+				"hwio",
+				bank|0x2000,
+				bank|0x7FFF,
+			)
+			if err != nil {
+				return
+			}
+		}
+	}
+
+	return
+}
+
+func (s *System) ReadWRAM24(offs uint32) uint32 {
+	lohi := uint32(binary.LittleEndian.Uint16(s.WRAM[offs : offs+2]))
+	bank := uint32(s.WRAM[offs+3])
+	return bank<<16 | lohi
+}
+
+func (s *System) ReadWRAM16(offs uint32) uint16 {
+	return binary.LittleEndian.Uint16(s.WRAM[offs : offs+2])
+}
+
+func (s *System) ReadWRAM8(offs uint32) uint8 {
+	return s.WRAM[offs]
+}
+
+func (s *System) SetPC(pc uint32) {
+	s.CPU.RK = byte(pc >> 16)
+	s.CPU.PC = uint16(pc & 0xFFFF)
+}
+
+func (s *System) GetPC() uint32 {
+	return uint32(s.CPU.RK)<<16 | uint32(s.CPU.PC)
+}
+
+func (s *System) RunUntil(targetPC uint32, maxCycles uint64) (stopPC uint32, expectedPC uint32, cycles uint64) {
+	expectedPC = targetPC
+	for cycles = uint64(0); cycles < maxCycles; {
+		if s.LoggerCPU != nil {
+			s.CPU.DisassembleCurrentPC(s.LoggerCPU)
+			fmt.Fprintln(s.LoggerCPU)
+		}
+		if s.GetPC() == targetPC {
+			break
+		}
+
+		nCycles, abort := s.CPU.Step()
+		cycles += uint64(nCycles)
+
+		if abort {
+			// fake that it's ok:
+			stopPC = s.GetPC()
+			expectedPC = s.GetPC()
+			return
+		}
+	}
+
+	stopPC = s.GetPC()
+	return
+}
+
+func (s *System) ExecAt(startPC, donePC uint32) (err error) {
+	s.SetPC(startPC)
+	return s.Exec(donePC)
+}
+
+func (s *System) Exec(donePC uint32) (err error) {
+	var stopPC uint32
+	var expectedPC uint32
+	var cycles uint64
+
+	if stopPC, expectedPC, cycles = s.RunUntil(donePC, 0x1000_0000); stopPC != expectedPC {
+		err = fmt.Errorf("CPU ran too long and did not reach PC=%#06x; actual=%#06x; took %d cycles", expectedPC, stopPC, cycles)
+		return
+	}
+
+	return
+}
+
+type DMARegs [16]byte
+
+func (c *DMARegs) ctrl() byte { return c[0] }
+func (c *DMARegs) dest() byte { return c[1] }
+func (c *DMARegs) srcL() byte { return c[2] }
+func (c *DMARegs) srcH() byte { return c[3] }
+func (c *DMARegs) srcB() byte { return c[4] }
+func (c *DMARegs) sizL() byte { return c[5] }
+func (c *DMARegs) sizH() byte { return c[6] }
+
+type DMAChannel struct{}
+
+func (c *DMAChannel) Transfer(regs *DMARegs, ch int, h *HWIO) {
+	aSrc := uint32(regs.srcB())<<16 | uint32(regs.srcH())<<8 | uint32(regs.srcL())
+	siz := uint16(regs.sizH())<<8 | uint16(regs.sizL())
+
+	bDest := regs.dest()
+	bDestAddr := uint32(bDest) | 0x2100
+
+	incr := regs.ctrl()&0x10 == 0
+	fixed := regs.ctrl()&0x08 != 0
+	mode := regs.ctrl() & 7
+
+	//if h.s.Logger != nil {
+	//	fmt.Fprintf(h.s.Logger, "PC=$%06x\n", h.s.GetPC())
+	//	fmt.Fprintf(h.s.Logger, "DMA[%d] start: $%06x -> $%04x [$%05x]\n", ch, aSrc, bDestAddr, siz)
+	//}
+
+	if regs.ctrl()&0x80 != 0 {
+		// PPU -> CPU
+		panic("PPU -> CPU DMA transfer not supported!")
+	} else {
+		// CPU -> PPU
+	copyloop:
+		for {
+			switch mode {
+			case 0:
+				h.Write(bDestAddr, h.s.Bus.EaRead(aSrc))
+				if !fixed {
+					if incr {
+						aSrc = ((aSrc&0xFFFF)+1)&0xFFFF + aSrc&0xFF0000
+					} else {
+						aSrc = ((aSrc&0xFFFF)-1)&0xFFFF + aSrc&0xFF0000
+					}
+				}
+				siz--
+				if siz == 0 {
+					break copyloop
+				}
+				break
+			case 1:
+				// p
+				h.Write(bDestAddr, h.s.Bus.EaRead(aSrc))
+				if !fixed {
+					if incr {
+						aSrc = ((aSrc&0xFFFF)+1)&0xFFFF + aSrc&0xFF0000
+					} else {
+						aSrc = ((aSrc&0xFFFF)-1)&0xFFFF + aSrc&0xFF0000
+					}
+				}
+				siz--
+				if siz == 0 {
+					break copyloop
+				}
+				// p+1
+				h.Write(bDestAddr+1, h.s.Bus.EaRead(aSrc))
+				if !fixed {
+					if incr {
+						aSrc = ((aSrc&0xFFFF)+1)&0xFFFF + aSrc&0xFF0000
+					} else {
+						aSrc = ((aSrc&0xFFFF)-1)&0xFFFF + aSrc&0xFF0000
+					}
+				}
+				siz--
+				if siz == 0 {
+					break copyloop
+				}
+				break
+			case 2:
+				panic("mode 2!!!")
+			case 3:
+				panic("mode 3!!!")
+			case 4:
+				panic("mode 4!!!")
+			case 5:
+				panic("mode 5!!!")
+			case 6:
+				panic("mode 6!!!")
+			case 7:
+				panic("mode 7!!!")
+			}
+		}
+	}
+
+	//if h.s.Logger != nil {
+	//	fmt.Fprintf(h.s.Logger, "DMA[%d]  stop: $%06x -> $%04x [$%05x]\n", ch, aSrc, bDestAddr, siz)
+	//}
+}
+
+type HWIO struct {
+	s *System
+
+	dmaregs [8]DMARegs
+	dma     [8]DMAChannel
+
+	ppu struct {
+		incrMode      bool   // false = increment after $2118, true = increment after $2119
+		incrAmt       uint32 // 1, 32, or 128
+		addrRemapping byte
+		addr          uint32
+	}
+
+	// mapped to $5000-$7FFF
+	Dyn [0x3000]byte
+}
+
+func (h *HWIO) Read(address uint32) (value byte) {
+	offs := address & 0xFFFF
+	if offs >= 0x5000 {
+		value = h.Dyn[offs-0x5000]
+		return
+	}
+
+	//if h.s.Logger != nil {
+	//	fmt.Fprintf(h.s.Logger, "hwio[$%04x] -> $%02x\n", offs, value)
+	//}
+	return
+}
+
+func (h *HWIO) Write(address uint32, value byte) {
+	offs := address & 0xFFFF
+
+	if offs == 0x4200 {
+		// NMITIMEN
+		return
+	}
+
+	if offs == 0x420b {
+		// MDMAEN:
+		hdmaen := value
+		//if h.s.Logger != nil {
+		//	fmt.Fprintf(h.s.Logger, "hwio[$%04x] <- $%02x DMA start\n", offs, hdmaen)
+		//}
+		// execute DMA transfers from channels 0..7 in order:
+		for c := range h.dma {
+			if hdmaen&(1<<c) == 0 {
+				continue
+			}
+
+			// channel enabled:
+			h.dma[c].Transfer(&h.dmaregs[c], c, h)
+		}
+		//if h.s.Logger != nil {
+		//	fmt.Fprintf(h.s.Logger, "hwio[$%04x] <- $%02x DMA end\n", offs, hdmaen)
+		//}
+		return
+	}
+	if offs == 0x420c {
+		// HDMAEN:
+		// no HDMA support
+		//if h.s.Logger != nil {
+		//	fmt.Fprintf(h.s.Logger, "hwio[$%04x] <- $%02x HDMA ignored\n", offs, value)
+		//}
+		return
+	}
+	if offs&0xFF00 == 0x4300 {
+		// DMA registers:
+		ch := (offs & 0x00F0) >> 4
+		if ch <= 7 {
+			reg := offs & 0x000F
+			h.dmaregs[ch][reg] = value
+		}
+
+		//if h.s.Logger != nil {
+		//	fmt.Fprintf(h.s.Logger, "hwio[$%04x] <- $%02x DMA register\n", offs, value)
+		//}
+		return
+	}
+
+	if offs == 0x2100 {
+		// INIDISP
+		return
+	}
+	if offs == 0x2102 || offs == 0x2103 {
+		// OAMADD
+		return
+	}
+	if offs == 0x2104 {
+		// OAMDATA
+		return
+	}
+	if offs == 0x2121 {
+		// CGADD
+		return
+	}
+	if offs == 0x2122 {
+		// CGDATA
+		return
+	}
+	if offs == 0x212e || offs == 0x212f {
+		// TMW, TSW
+		return
+	}
+
+	// PPU:
+	if offs == 0x2115 {
+		// VMAIN = o---mmii
+		h.ppu.incrMode = value&0x80 != 0
+		switch value & 3 {
+		case 0:
+			h.ppu.incrAmt = 1
+			break
+		case 1:
+			h.ppu.incrAmt = 32
+			break
+		default:
+			h.ppu.incrAmt = 128
+			break
+		}
+		h.ppu.addrRemapping = (value & 0x0C) >> 2
+		if h.ppu.addrRemapping != 0 {
+			panic(fmt.Errorf("unsupported VRAM address remapping mode %d", h.ppu.addrRemapping))
+		}
+		//if h.s.Logger != nil {
+		//	fmt.Fprintf(h.s.Logger, "PC=$%06x\n", h.s.GetPC())
+		//	fmt.Fprintf(h.s.Logger, "VMAIN = $%02x\n", value)
+		//}
+		return
+	}
+	if offs == 0x2116 {
+		// VMADDL
+		h.ppu.addr = uint32(value) | h.ppu.addr&0xFF00
+		//if h.s.Logger != nil {
+		//	fmt.Fprintf(h.s.Logger, "PC=$%06x\n", h.s.GetPC())
+		//	fmt.Fprintf(h.s.Logger, "VMADDL = $%04x\n", h.ppu.addr)
+		//}
+		return
+	}
+	if offs == 0x2117 {
+		// VMADDH
+		h.ppu.addr = uint32(value)<<8 | h.ppu.addr&0x00FF
+		//if h.s.Logger != nil {
+		//	fmt.Fprintf(h.s.Logger, "PC=$%06x\n", h.s.GetPC())
+		//	fmt.Fprintf(h.s.Logger, "VMADDH = $%04x\n", h.ppu.addr)
+		//}
+		return
+	}
+	if offs == 0x2118 {
+		// VMDATAL
+		h.s.VRAM[h.ppu.addr<<1] = value
+		if h.ppu.incrMode == false {
+			h.ppu.addr += h.ppu.incrAmt
+		}
+		return
+	}
+	if offs == 0x2119 {
+		// VMDATAH
+		h.s.VRAM[(h.ppu.addr<<1)+1] = value
+		if h.ppu.incrMode == true {
+			h.ppu.addr += h.ppu.incrAmt
+		}
+		return
+	}
+
+	// APU:
+	if offs >= 0x2140 && offs <= 0x2143 {
+		// APUIO0 .. APUIO3
+		return
+	}
+
+	if h.s.Logger != nil {
+		fmt.Fprintf(h.s.Logger, "hwio[$%04x] <- $%02x\n", offs, value)
+	}
+}
+
+func (h *HWIO) Shutdown() {
+}
+
+func (h *HWIO) Size() uint32 {
+	return 0x10000
+}
+
+func (h *HWIO) Clear() {
+}
+
+func (h *HWIO) Dump(address uint32) []byte {
+	return nil
+}
